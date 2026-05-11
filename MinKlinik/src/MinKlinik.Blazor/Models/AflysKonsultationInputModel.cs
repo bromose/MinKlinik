afae8bb5c23481ef7eb37359cdf3b3cef2f6ade8
Kaj Bromose <kbr@ucl.dk>
2026-05-11 14:23:48 +0200

    kap-13

--- MinKlinik/src/MinKlinik.Blazor/Models/AflysKonsultationInputModel.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace MinKlinik.Blazor.Models;
-
-public class AflysKonsultationInputModel
-{
-    [Required]
-    public Guid? KonsultationId { get; set; }
-}
